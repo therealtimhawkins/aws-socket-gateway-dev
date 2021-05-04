68263c7927713ff87d350d13c1f00f99bb7a55c7
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2021-05-04 22:37:26 +0100

    adds request to lambda handler on message

--- socket.js
+++ socket.js
@@ -2,7 +2,9 @@ require("dotenv").config()
 const WebSocket = require("ws")
 const short = require("short-uuid")
 
+const lambda = require(process.env.LAMBDA_PATH)
 const connections = {}
+
 const send = (connectionId, data) => {
   const connection = connections[connectionId]
   connection.send(data)
@@ -26,8 +28,14 @@ const wss = new WebSocket.Server({ port: process.env.SOCKET_PORT })
 
 wss.on("connection", (socket) => {
   defaultActions.connect(socket)
-  socket.on("message", (message) => {
-    console.log({ message })
+  socket.on("message", (data) => {
+    const event = {
+      body: JSON.stringify(data),
+      requestContext: {
+        connectionId: socket.connectionId,
+      },
+    }
+    lambda.handler(event)
   })
   socket.on("close", () => {
     defaultActions.disconnect(socket.connectionId)
